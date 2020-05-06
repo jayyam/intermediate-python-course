1126d3ad0bc70fc0abe66e56b29f440436fe14ab
root <root@shaynet.shaydomain>
2020-05-06 14:54:32 +0200

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -2,17 +2,18 @@ def main():
   import random #biblioteca funciones random
   
   dice_sum = 0 #variable suma de dados
-  dice_rolls = 2 #variable dos dados
+  dice_rolls = int(input('How many dice would you like to roll? ')) #variable cantidad de dados
+  dice_size = int(input('How many sides are in the dice?) ')) #variable tamaño de cada uno de dados
   
   for i in range (0,dice_rolls):
-    roll = random.randint(1,6) #asigna numero random a dado
+    roll = random.randint(1,dice_size) #asigna numero random a dado
     dice_sum += roll #operacion de suma de valores de dados (dice_sum = dice_sum + roll)
     if roll == 1:
         print(f'You rolled a {roll}! Critical Fail')#imprime resultado Critical 1*
-    elif roll == 6:
-        print(f'You rolled a {roll}! Critical Success')#imprime resultado Critical 6*
+    elif roll == dice_size:
+        print(f'You rolled a {roll}! Critical Success')#imprime resultado Critical 20*
     else:
-        print(f'You rolled a {roll}')#imprime resultado dados individualmente
+        print(f'You rolled a {roll}')#imprime resultado de dados individualmente
   print(f'You have rolled a total of {dice_sum}') #imprime la suma de los dados
 
 if __name__== "__main__":
