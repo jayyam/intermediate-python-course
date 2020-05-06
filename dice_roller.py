f58a398d431752f45ee578ba18e05e924581017b
root <root@shaynet.shaydomain>
2020-05-06 14:41:32 +0200

    Now rolling two dice adding Criticals

--- dice_roller.py
+++ dice_roller.py
@@ -7,8 +7,12 @@ def main():
   for i in range (0,dice_rolls):
     roll = random.randint(1,6) #asigna numero random a dado
     dice_sum += roll #operacion de suma de valores de dados (dice_sum = dice_sum + roll)
-    print(f'You rolled a {roll}')#imprime resultado dados individualmente
-    
+    if roll == 1:
+        print(f'You rolled a {roll}! Critical Fail')#imprime resultado Critical 1*
+    elif roll == 6:
+        print(f'You rolled a {roll}! Critical Success')#imprime resultado Critical 6*
+    else:
+        print(f'You rolled a {roll}')#imprime resultado dados individualmente
   print(f'You have rolled a total of {dice_sum}') #imprime la suma de los dados
 
 if __name__== "__main__":
